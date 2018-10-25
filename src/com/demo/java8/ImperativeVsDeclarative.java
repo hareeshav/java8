8a63e8dde17ae394c65e1b9d614c186d8b873282
hareev <hareev@INBEN10229.kongsberg.master.int>
2018-10-25 13:00:43 +0530

    Imperative vs decalarative example
    Change-Id: Id47cc385c060cd5f7b8eb1a6e94d88e3234f0837

---
+++ src/com/demo/java8/ImperativeVsDeclarative.java
@@ -0,0 +1,24 @@
+package com.demo.java8;
+
+import java.util.stream.IntStream;
+
+/*
+ * This class demonstrates the difference between imperative and declarative approach using sum of 1 to 100 nos example.
+ */
+public class ImperativeVsDeclarative {
+	
+	public static void main(String[] args) {
+		//Imperative: How style of programming, how we get the result.
+		
+		int sum = 0;
+		for(int i=0; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println("Sum of nos using imperative approach: "+sum);
+		
+		//Declarative: What style of programming, what result we need. Use parallel to make it parallel.
+		int sum1 = IntStream.rangeClosed(0, 100).parallel().sum();
+		System.out.println("Sum of nos using declarative approach: "+sum1);
+	}
+
+}
